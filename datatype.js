2c3ae9119d224ec9e26ef017a2562ced20b20aa0
Syeda-Shanzay <syedashanzay04@gmail.com>
2024-03-24 00:52:43 +0500

    first commit

---
+++ datatype.js
@@ -0,0 +1,61 @@
+// // Primitive Data Types:
+// // 1# String Data Type:
+// // 'Single Quote'
+// let user1 : string = "Syeda" 
+// console.log(user1)
+// // "Double Quote"
+// let user2 : string = 'Syeda'
+// console.log(user2)
+// // `Backticks`
+// let user3 : string = `Syeda`
+// console.log(user3)
+// // 2# Number Data Type:
+// let phone1 : number = 50
+// console.log(phone1)
+// let phone2 : number = 0
+// console.log(phone2)
+// let phone3 : number = 1.230
+// console.log(phone3)
+// // 3# Boolean:
+// let is_user_student : boolean = true
+// console.log(is_user_student)
+// // 4# Any:
+// let generic_variable : any = 100
+// console.log(generic_variable)
+// // Additional Primitive Data Types:
+// // 1# BigInt:
+// let bigNum = BigInt(1234567899999999)
+// console.log(bigNum)
+// // 2# Unknown:
+// let user_name : unknown = "Ali"
+// console.log(user_name)
+// // 3# Null:
+// let user_location = null
+// console.log(user_location)
+// // 4# Undefined:
+// let myAge
+// console.log(myAge)
+// // Strongly Typed Syntax:
+// let a : string = "Pakistan"
+// console.log(a)
+// let b : number = 10
+// console.log(b)
+// let c : boolean = true
+// console.log(c)
+// //  Type Inference:
+// let d = "USA"
+// console.log(d)
+// let e = 10.8
+// console.log(typeof(e))
+// let f = false
+// console.log(f)
+// //  String Concatenation:
+// let first_name : string = 'Syeda'
+// let last_name : string = "Shanzay"
+// let full_name : string = first_name + ' ' + last_name
+// console.log(full_name)
+// Template Literals:
+var name1 = "Syeda";
+var name2 = "Shanzay";
+var name3 = "".concat(name1, " ").concat(name2);
+console.log(name3);
